082695284176a87185939e26e213a5266ca67d8f
jazzwall <sharad@jazzwall.com>
2017-03-27 10:20:17 -0700

    Modded hello tf

--- hellotf.py
+++ hellotf.py
@@ -1,7 +1,16 @@
 import tensorflow as tf
 
 hello_constant = tf.constant('Hello World')
-
+A = tf.constant(1234)
+AA = tf.constant(1111)
+AAA = tf.add(A, AA)
+B = tf.constant([123,456,789])
+C = tf.constant([[123,456,789], [222,333,444]])
+D = tf.add(B, C)
 with tf.Session() as sess:
     output = sess.run(hello_constant)
     print(output)
+    numoutput = sess.run(AAA)
+    print (numoutput)
+    matoutput = sess.run(D)
+    print(D)
