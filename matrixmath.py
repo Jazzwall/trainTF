6ea2ba9806639445b10c8d27fe71121063a35612
jazzwall <sharad@jazzwall.com>
2017-03-30 11:33:07 -0700

    tf matrix math complete

--- matrixmath.py
+++ matrixmath.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import tensorflow as tf
-
+#### NUMPY MATRIX MATH
 # wx + b using numpy
 w = np.array([[-.5, .2, .1],[.7,-.8,.2]])
 x = np.array([[.2],[.5],[.6]])
@@ -30,3 +30,17 @@ print(xw)
 resulttranspose = xw + bt
 print("Result transposed -------")
 print(resulttranspose)
+
+#### TENSORFLOW MATRIX MATH
+print("----------- Tensorflow Stuff --------------")
+
+xtf = tf.constant(xt)
+wtf = tf.Variable(wt)
+btf = tf.Variable(bt)
+xwtf = tf.matmul(xtf, wtf)
+ytf =  tf.add(xwtf, btf)
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    tfresult = sess.run(ytf)
+    print(tfresult)
